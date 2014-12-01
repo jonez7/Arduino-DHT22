cd8c39630884b0d22da218a2821545c3eda403c6
Jouni Seppanen <jouni.seppanen@pehutec.com>
2014-12-01 22:22:09 +0200

    added support for dew point calculation

--- DHT22.h
+++ DHT22.h
@@ -2,6 +2,7 @@
 #define _DHT22_H_
 
 #include <inttypes.h>
+#include <math.h>
 
 #define DHT22_ERROR_VALUE -995
 
@@ -27,9 +28,14 @@ class DHT22
     public:
         DHT22(uint8_t const pin);
         DHT22_ERROR_t readData();
-        inline float getTemperatureF() { return float(_lastTemperature)/10 * 9 / 5 + 32; }
-        inline float getTemperatureC() { return float(_lastTemperature)/10; }
-        inline float getHumidity() { return float(_lastHumidity)/10; }
+        inline float getTemperatureF() const { return float(_lastTemperature)/10 * 9 / 5 + 32; }
+        inline float getTemperatureC() const { return float(_lastTemperature)/10; } 
+        inline float getHumidity() const { return float(_lastHumidity)/10; }
+        /* Approximate dew point in Celcius */
+        inline float getDewPointC() const { 
+            float const H = (log10(getHumidity()) - 2) / 0.4343 + (17.62 * getTemperatureC())/(243.12 + getTemperatureC());
+            return (243.12*H/(17.62-H));
+        }
 };
 
 
